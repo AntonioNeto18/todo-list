2fae0fed258dbb1a5bb9b96c578d82ec1c18c730
Antonio <antoniojacintoa@gmail.com>
2026-02-01 16:48:13 -0300

    style: alterando a cor do status da todo no frontend

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -8,23 +8,30 @@ const Todo = ({ todo, handleDeleteTodos, onOpenModal }) => {
                 className="flex flex-col"
             >
                 <span className="text-3xl font-semibold">{todo.text}</span>
-                <span className="text-lg capitalize">{todo.category.toLowerCase()}</span>
+                <span
+                    className={"text-lg font-bold capitalize"}
+                >
+                    {todo.category}
+                </span>
             </div>
 
             <div className="flex items-center">
 
                 <div className="flex flex-col items-center gap-2">
-                    <p className="capitalize text-lg">
-                        <span className="font-semibold">Status: </span>
+                    <p className={`capitalize text-lg font-bold ${todo.status.toLowerCase() === "pendente"
+                            ? "text-red-500"
+                            : "text-green-500"
+                        }`}>
+                        <span className="font-semibold text-black">Status: </span>
                         {todo.status.toLowerCase()}
                     </p>
 
                     <button
-                      className="bg-red-500 text-white text-lg uppercase w-fit h-fit text-center px-5 py-2 rounded-2xl cursor-pointer z-10 border-2 border-red-600 hover:bg-red-400 transition-all duration-300"
-                      onClick={(e) => {
-                        e.stopPropagation()
-                        handleDeleteTodos(todo.id)
-                      }}
+                        className="bg-red-500 text-white text-lg uppercase w-fit h-fit text-center px-5 py-2 rounded-2xl cursor-pointer z-10 border-2 border-red-600 hover:bg-red-400 transition-all duration-300"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            handleDeleteTodos(todo.id)
+                        }}
                     >
                         Deletar
                     </button>
